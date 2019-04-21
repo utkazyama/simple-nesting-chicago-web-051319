dc965276236651588bad7f00082b4855e489f5c3
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-21 05:50:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {}
-		 
+     programmer_hash[:yukihiro_matsumoto] = {}
 		 programmer_hash
 end
 
@@ -105,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for]="GUI"
      
 end
 
